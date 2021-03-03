1874b7eef2642a48e228e67e75d014c2e2ef13b1
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-02 20:19:12 -0500

    updated search page

--- client/src/Pages/Search.js
+++ client/src/Pages/Search.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BookSearch from "../components/BookSearch/BookSearch";
 import BookResult from "../components/BookResult/BookResult";
+import axios from "axios";
 
 
-
+function BookInfo(book) {
+  return (
+    // <BookSearch />
+    console.log()
+  );
+}
 
 const Search = () => {
+  const [books, setBooks] = useState([]);
+
+  function SearchBooks() {
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=intitle:romeo&key=AIzaSyAfrpQXZXcliTIXdDGXtxQAW6Pi5iGb42w`
+      )
+      .then((response) => {
+        console.log(response.data.items);
+        setBooks(response.data.items);
+      });
+  }
+
+  function saveBook(book) {
+    axios
+      .post("/api/books", book)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    SearchBooks();
+  }, []);
+
   return (
     <div className="container">
       <div className="row">
@@ -14,7 +48,10 @@ const Search = () => {
             <h1>Book Search</h1>
             <BookSearch />
           </div>
-          <BookResult />
+          <h1 className="mt-3">Results</h1>
+          {books.map((book) => (
+            <BookResult book={book} key={book.id} saveBook={saveBook} />
+          ))}
         </div>
       </div>
     </div>
