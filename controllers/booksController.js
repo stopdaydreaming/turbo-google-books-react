fb3731a03b9198fd2c7fd7e73b767e7268e1eb24
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-02 20:22:09 -0500

    created books controller

---
+++ controllers/booksController.js
@@ -0,0 +1,40 @@
+const db = require("../models");
+
+// Defining methods for the booksController
+
+//exports an object
+module.exports = {
+  findAll: function (req, res) {
+    db.Book.find(req.query)
+      .sort({ date: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.Book.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    console.log(req.body)
+    db.Book.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Book.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+    })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => {
+        console.log(err);
+        res.status(422).json(err);
+    });
+  },
+  remove: function (req, res) {
+    db.Book.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
