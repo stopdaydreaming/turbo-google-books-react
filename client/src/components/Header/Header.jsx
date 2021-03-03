57c3c388f20f1dc9c462348d8c965e59ec2c6a6b
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-02 19:22:26 -0500

    created header component

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Google Books
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Search
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/saved">
+                  Saved
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
