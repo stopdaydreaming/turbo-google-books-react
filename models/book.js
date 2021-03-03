22cb0e3b89105d1c98aef11facb935a13ab67afe
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-02 22:20:41 -0500

    added comment to book js file

--- models/book.js
+++ models/book.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+//only expecting the following properties
 const bookSchema = new Schema({
-  authors: { type: String, required: true },
-  description: { type: String, required: true },
+  authors: [{ type: String, required: true }],
+  description: { type: String, required: false },
   image: { type: String, required: true },
   link: { type: String, required: true },
   title: { type: String, required: true },
