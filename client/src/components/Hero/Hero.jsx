275e2fe4f9733c8aae0f75c2603338a04aef94fa
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-02 19:23:21 -0500

    created hero component

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    
+      <div className="jumbotron">
+        <h1 className="display-4 text-center">React Google Book Search</h1>
+        <hr className="my-4" />
+        <p className="lead text-center">
+          Search for and save books of interest.
+        </p>
+    </div>
+    
+  );
+};
+
+export default Hero;
