a3876669ffd5c0c321eae19b80dc59972df3aed1
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-02 20:21:17 -0500

    created functions

--- client/src/components/BookSearch/BookSearch.jsx
+++ client/src/components/BookSearch/BookSearch.jsx
@@ -1,46 +1,48 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-
-// const query =  https://www.googleapis.com/books/v1/volumes?q=intitle:{formValue}&key=AIzaSyAfrpQXZXcliTIXdDGXtxQAW6Pi5iGb42w
-
-const SearchBooks = () => {
-
-  axios
-    .get(
-      `https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=AIzaSyAfrpQXZXcliTIXdDGXtxQAW6Pi5iGb42w`
-    )
-    .then((response) => {
-       console.log(response);
-      //  console.log(response.data.items[0].volumeInfo.title);
-      //  console.log(response.data.items[0].volumeInfo.authors[0]);
-      //  console.log(response.data.items[0].volumeInfo.imageLinks.thumbnail);
-    });
-}
-
-const BookSearch = () => {
-
-  const [bookSearch, setBookSearch] = useState("");
-
-  const handleInputChange = event => {
-    const { value} = event.target;
-    setBookSearch(value)
-  }
+import axios from "axios"
 
 
-  useEffect(() => {
-    SearchBooks();
-  }, []);
+// function SearchBooks(e) {
+//   e.preventDefault()
+ 
+//   axios
+//     .get(
+//       `https://www.googleapis.com/books/v1/volumes?q=intitle:&key=AIzaSyAfrpQXZXcliTIXdDGXtxQAW6Pi5iGb42w`
+//     )
+//     .then((response) => {
+//        console.log(response.data.items);
+
+//     });
+// }
+
+
+ const BookSearch = () => {
+
+   const [bookSearch, setBookSearch] = useState("");
+
+   const handleFormSubmit = event => {
+     event.preventDefault();
+  console.log("I was submitted")
+     const { value} = event.target;
+     console.log(value)
+     //setBookSearch(value)
+   }
+
+
+//   useEffect(() => {
+    
+//   }, []);
 
   return (
     <div className="mb-5">
-      <form onSubmit={SearchBooks()}>
+      <form onSubmit={handleFormSubmit}>
         <div className="mb-3">
           <label htmlFor="searchBook" className="form-label">
             Search for Book
           </label>
           <input
-            onChange={handleInputChange}
-            value={bookSearch}
+            // onChange={handleInputChange}
+            // value={bookSearch}
             type="text"
             className="form-control mb-3 "
             id="searchBook"
