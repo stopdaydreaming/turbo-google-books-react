25b4b063e11b208a07fff5ecb135109afa2efec7
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-02 22:18:14 -0500

    setup save book functionality

--- client/src/Pages/Search.js
+++ client/src/Pages/Search.js
@@ -1,44 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import BookSearch from "../components/BookSearch/BookSearch";
 import BookResult from "../components/BookResult/BookResult";
 import axios from "axios";
 
-
-function BookInfo(book) {
-  return (
-    // <BookSearch />
-    console.log()
-  );
-}
-
 const Search = () => {
   const [books, setBooks] = useState([]);
 
-  function SearchBooks() {
+  const saveBook = (book) => {
+    const bookResults = {
+      authors: book.authors,
+      description: book.description,
+      image: book.imageLinks.thumbnail,
+      link: book.infoLink,
+      title: book.title
+    };
     axios
-      .get(
-        `https://www.googleapis.com/books/v1/volumes?q=intitle:romeo&key=AIzaSyAfrpQXZXcliTIXdDGXtxQAW6Pi5iGb42w`
-      )
-      .then((response) => {
-        console.log(response.data.items);
-        setBooks(response.data.items);
-      });
-  }
-
-  function saveBook(book) {
-    axios
-      .post("/api/books", book)
-      .then((response) => {
+      .post("/api/books", bookResults)
+      .then(response => {
         console.log(response);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
-  }
-
-  useEffect(() => {
-    SearchBooks();
-  }, []);
+  };
 
   return (
     <div className="container">
@@ -46,10 +30,10 @@ const Search = () => {
         <div className="col-md-12">
           <div className="bg-light p-5">
             <h1>Book Search</h1>
-            <BookSearch />
+            <BookSearch setBooks={setBooks} />
           </div>
           <h1 className="mt-3">Results</h1>
-          {books.map((book) => (
+          {books.map(book => (
             <BookResult book={book} key={book.id} saveBook={saveBook} />
           ))}
         </div>
