c3fb09d85a5b38c029bd0a73bd83b474bb13a1f8
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-02 22:19:22 -0500

    setup book search component

--- client/src/components/BookSearch/BookSearch.jsx
+++ client/src/components/BookSearch/BookSearch.jsx
@@ -1,37 +1,31 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios"
+import axios from "axios";
 
+const BookSearch = props => {
+  const [bookSearch, setBookSearch] = useState("");
 
-// function SearchBooks(e) {
-//   e.preventDefault()
- 
-//   axios
-//     .get(
-//       `https://www.googleapis.com/books/v1/volumes?q=intitle:&key=AIzaSyAfrpQXZXcliTIXdDGXtxQAW6Pi5iGb42w`
-//     )
-//     .then((response) => {
-//        console.log(response.data.items);
+  const searchBooks = value => {
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=intitle:${value}&key=AIzaSyAfrpQXZXcliTIXdDGXtxQAW6Pi5iGb42w`
+      )
+      .then(response => {
+        console.log(response.data.items);
+        props.setBooks(response.data.items);
+      });
+  };
 
-//     });
-// }
+  const handleInputChange = e => {
+    const { value } = e.target;
+    console.log(value);
+    setBookSearch(value);
+  };
 
-
- const BookSearch = () => {
-
-   const [bookSearch, setBookSearch] = useState("");
-
-   const handleFormSubmit = event => {
-     event.preventDefault();
-  console.log("I was submitted")
-     const { value} = event.target;
-     console.log(value)
-     //setBookSearch(value)
-   }
-
-
-//   useEffect(() => {
-    
-//   }, []);
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    console.log("I was submitted");
+    searchBooks(bookSearch);
+  };
 
   return (
     <div className="mb-5">
@@ -41,8 +35,8 @@ import axios from "axios"
             Search for Book
           </label>
           <input
-            // onChange={handleInputChange}
-            // value={bookSearch}
+            onChange={handleInputChange}
+            value={bookSearch}
             type="text"
             className="form-control mb-3 "
             id="searchBook"
