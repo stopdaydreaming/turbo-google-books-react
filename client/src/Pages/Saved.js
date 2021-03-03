3a40a93cb67af8b1bec93845bd8fd0d86f796dd9
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-02 19:14:58 -0500

    added saved page

---
+++ client/src/Pages/Saved.js
@@ -0,0 +1,17 @@
+import React from "react";
+import SavedBooks from "../components/SavedBooks/SavedBooks";
+
+const Saved = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Saved Books</h1>
+          <SavedBooks />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Saved;
