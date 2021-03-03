d26324fea2107088c1a3467684c15e17ba231caf
stopdaydreaming <hello@stopdaydreaming.com>
2021-03-02 22:17:28 -0500

    setup saved books component

--- client/src/Pages/Saved.js
+++ client/src/Pages/Saved.js
@@ -1,13 +1,34 @@
-import React from "react";
-//import SavedBooks from "../components/SavedBooks/SavedBooks";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SavedBooks from "../components/SavedBooks/SavedBooks";
 
 const Saved = () => {
+  const [books, setBooks] = useState([]);
+
+  const getBooks = () => {
+    axios
+      .get("/api/books")
+      .then(res => {
+        console.log(res);
+        setBooks(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-12">
           <h1>Saved Books</h1>
-          {/* <SavedBooks /> */}
+          {books.map(book => (
+            <SavedBooks book={book} key={book._id} />
+          ))}
         </div>
       </div>
     </div>
